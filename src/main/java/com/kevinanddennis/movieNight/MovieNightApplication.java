9cd4a9a02cf775fa838b3f65c929b35c8043781e
unknown <retzke66@yahoo.de>
2020-11-07 16:50:38 +0100

    init

---
+++ src/main/java/com/kevinanddennis/movieNight/MovieNightApplication.java
@@ -0,0 +1,13 @@
+package com.kevinanddennis.movieNight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieNightApplication.class, args);
+	}
+
+}
